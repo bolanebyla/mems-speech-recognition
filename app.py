a98be17906ca87b8e16029e9db6603d20e2814fe
bolanebyla <alexleskov2000@gmail.com>
2020-07-03 16:09:45 +0800

    Попытка перезаписать файл

--- app.py
+++ app.py
@@ -1,25 +1,48 @@
+import os
+import subprocess
 import flask
 import pickle
+import ffmpeg
 
 import numpy as np
+from flask import request
+from flask import jsonify
+from flask import render_template
+from werkzeug.utils import secure_filename
 
 from model import get_model, get_prediction
+from sound import get_audio
 
-app = flask.Flask(__name__, template_folder='templates')
+app = flask.Flask(__name__, template_folder='templates', static_folder='static')
 
 CLASSES = ['ГРУСТЬ', 'МЕМ']  # Классы команд (без фона)
 
 model = get_model()
 model.load_weights("model-cnn.h5")
 
+uploads_dir = os.path.join(app.instance_path, 'uploads')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # if flask.request.method == 'GET':
-    # if flask.request.method == 'POST':
-    wavfiles = 'одинокий мем 4.wav'  # Получаем имя очередного файла
-    print(get_prediction(wavfiles, model, CLASSES))
-    return f'<h1>{get_prediction(wavfiles, model, CLASSES)}</h1>'
+    if flask.request.method == 'GET':
+        return render_template('index.html')
+
+    if flask.request.method == 'POST':
+        voice = request.files.get('voice')
+        print(voice)
+        voice.save('uploads/recording.wav')
+        print('uploaded')
+
+        #subprocess.call(['ffmpeg', '-i', 'uploads/recording.wav',
+         #                'uploads/recording1.wav'])
+
+        #get_audio(voice.read())
+
+        wavfiles = 'uploads/1.wav'  # Получаем имя очередного файла
+        print(get_prediction(wavfiles, model, CLASSES))
+
+    return render_template('index.html', result=get_prediction(wavfiles, model, CLASSES))
 
 
 if __name__ == '__main__':
--- sound.py
+++ sound.py
@@ -1,13 +1,15 @@
 import librosa
 import numpy as np
-
-
+import ffmpeg
+from base64 import b64decode
+import io
+from scipy.io.wavfile import read as wav_read
 
 
 def wav2mfcc(file_path, length=11025, step=2205):
     out_mfcc = []  # Выходной массив, содержащий mfcc исходного файла с шагом step
     out_audio = []  # Выходной массив, содеражищий аудиоинформацию исходного файла с шагом step
-    y, sr = librosa.load(file_path)  # Загружаем данные исходного файла
+    y, sr = librosa.load(file_path, duration=5.0)  # Загружаем данные исходного файла
 
     while (len(
             y) >= length):  # Проходим весь массив y, пока оставшийся кусочек не станет меньше указанной в параметре max_len длинны
@@ -22,3 +24,39 @@ def wav2mfcc(file_path, length=11025, step=2205):
     out_audio = np.array(out_audio)  # Преобразуем в numpy
     return out_mfcc, out_audio  # функция вернет массив мэл-частот и массив аудио-отре
 
+
+def get_audio(data):  # объявляем функцию извлечения аудио, записанного через микрофон в ноутбуке
+    print(data)
+    # data сейчас в таком виде: 'data:audio/webm;codecs=opus;base64,GkXfo59ChoEBQveBAULygQRC84EIQoKEd2VibUKHgQRChYE...mpZpkq1'
+    binary = b64decode(
+        data)  # отсекаем информацию "data:audio/webm;codecs=opus;base64", оставляем только данные и декодируем
+    # b'\x1aE\xdf\xa3\x9fB\x86\x81\x01B\xf7\x81\x01B\xf2
+
+    process = (ffmpeg
+               .input('pipe:0')  # поток стандартного ввода
+               .output('pipe:1', format='wav')  # стандартного вывода в формате wav
+               .run_async(pipe_stdin=True, pipe_stdout=True, pipe_stderr=True, quiet=True,
+                          overwrite_output=True))  # асинхронное выполнение командной строки FFmpeg
+    print('nen')
+    # binary преобразовываем в wav, с типичным для медиафайлов форматом RIFF(ResourceInterchangeFileFormat - формат файла для обмена ресурсами)
+    output, err = process.communicate(input=binary)
+    # output - b'RIFF\xff\xff\xff\xffWAVEfmt \x10\x00\x00\x00\x01\x00\x01\x00\x80\xbb\x00\x00\...\x01\x00\x01\x00\x01\x0\xff'
+    # err - b"ffmpeg version 3.4.6-0ubuntu0.18.04.1 Copyright (c) 2000-2019 the FFmpeg developers\n...headers:0kB muxing overhead: 0.046695%\n"
+
+    # Cодержимое файла группируется из отдельных секций (chunks) - формат выборок аудиоданных. Секция имеет свой заголовок и данные секции.
+    # Размер заголовка секции RIFF - 8 байт, их ниже уберём из определения размера секции RIFF
+    riff_chunk_size = len(output) - 8
+    # Разбиваем размер секции на четыре байта, которые запишем далее в b.
+    q = riff_chunk_size
+    b = []
+    for i in range(4):
+        q, r = divmod(q, 256)  # возьмем размер секции и вернем частное и остаток от деления на 256, и так 4 раза
+        b.append(r)  # каждый из остатков добавим в список
+
+    # Меняем байты c 4го по 7й вкл-но('\xff\xff\xff\xff') в output на b (типа '\xc62\x02\x00')
+    riff = output[:4] + bytes(b) + output[8:]
+
+    # класс io.BytesIO позволит работать с последовательностью байтов как с файловым объектом, а далее прочитаем как wav файл
+    sr, audio = wav_read(io.BytesIO(riff))  # извлечём частоту дискретизации и полученный сигнал
+
+    return audio, sr  # функция вернет полученный сигнал и частоту дискретизации
