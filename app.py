f1589a75399abf88fb5d52a6dc10a9b3d5570f1f
bolanebyla <alexleskov2000@gmail.com>
2020-07-06 14:34:35 +0800

    Обработка аудио и отправка пердикта

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 import os
 import subprocess
 import flask
+import json
 import pickle
-import ffmpeg
 
-import numpy as np
 from flask import request
-from flask import jsonify
 from flask import render_template
-from werkzeug.utils import secure_filename
 
 from model import get_model, get_prediction
-from sound import get_audio
 
 app = flask.Flask(__name__, template_folder='templates', static_folder='static')
 
@@ -30,19 +26,19 @@ def index():
 
     if flask.request.method == 'POST':
         voice = request.files.get('voice')
-        print(voice)
-        voice.save('uploads/recording.wav')
-        print('uploaded')
+        voice.save('uploads/recording.wav')  # Сохраняем аудио
+        subprocess.call(
+            ['ffmpeg',
+             '-i',
+             'uploads/recording.wav',
+             '-y',
+             '-hide_banner'])  # Перезаписываем с нужной кодировкой
 
-        #subprocess.call(['ffmpeg', '-i', 'uploads/recording.wav',
-         #                'uploads/recording1.wav'])
-
-        #get_audio(voice.read())
-
-        wavfiles = 'uploads/1.wav'  # Получаем имя очередного файла
-        print(get_prediction(wavfiles, model, CLASSES))
-
-    return render_template('index.html', result=get_prediction(wavfiles, model, CLASSES))
+        wavfiles = 'uploads/recording.wav'  # Имя очередного файла
+        prediction = get_prediction(wavfiles, model, CLASSES)  # Предсказание модели
+        print(prediction)
+        data = json.dumps({'data': prediction})  # Записываем в json
+        return data
 
 
 if __name__ == '__main__':
