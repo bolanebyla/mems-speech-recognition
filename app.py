51e55fc56fe1fd929ac5645cbb3fafd318112903
bolanebyla <alexleskov2000@gmail.com>
2020-07-07 20:33:16 +0800

    Попытка сделать бота и сайт вместе

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import os
-import subprocess
 import flask
 import json
 import pickle
 
 from flask import request
 from flask import render_template
+from bot import *
 
-from model import get_model, get_prediction
+#from model import get_model, get_prediction
 
 app = flask.Flask(__name__, template_folder='templates', static_folder='static')
 
 CLASSES = ['ГРУСТЬ', 'МЕМ']  # Классы команд (без фона)
 
-model = get_model()
-model.load_weights("model-cnn.h5")
+#model = get_model()
+#model.load_weights("model-cnn.h5")
 
 uploads_dir = os.path.join(app.instance_path, 'uploads')
 
@@ -26,20 +26,25 @@ def index():
 
     if flask.request.method == 'POST':
         voice = request.files.get('voice')
-        voice.save('uploads/recording.wav')  # Сохраняем аудио
+        voice.save('uploads/web/recording.wav')  # Сохраняем аудио
         subprocess.call(
             ['ffmpeg',
              '-i',
-             'uploads/recording.wav',
+             'uploads/web/recording.wav',
              '-y',
              '-hide_banner'])  # Перезаписываем с нужной кодировкой
 
-        wavfiles = 'uploads/recording.wav'  # Имя очередного файла
-        prediction = get_prediction(wavfiles, model, CLASSES)  # Предсказание модели
+        wavfiles = 'uploads/web/recording.wav'  # Имя очередного файла
+        #prediction = get_prediction(wavfiles, model, CLASSES)  # Предсказание модели
+        prediction = 'тестовый предикт web'
         print(prediction)
         data = json.dumps({'data': prediction})  # Записываем в json
         return data
 
+@app.route('/bot', methods=['GET', 'POST'])
+def webhook(request):
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return 'ok', 200
 
 if __name__ == '__main__':
     app.run()
--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 import telebot
 import requests
 import subprocess
-from model import get_model, get_prediction
+#from model import get_model, get_prediction
 
 TOKEN = '1378189568:AAFcZV-g3lmahSZlcB1_9PnerNrTiVNvo_4'
+HOST = '341088-cs07173.tmweb.ru/'
 bot = telebot.TeleBot(TOKEN, threaded=False)
+bot.set_webhook(url=HOST + 'bot/' + TOKEN)
 
 CLASSES = ['ГРУСТЬ', 'МЕМ']  # Классы команд (без фона)
 
-model = get_model()
-model.load_weights("model-cnn.h5")
+#model = get_model()
+#model.load_weights("model-cnn.h5")
 
 
 # Команда /start
@@ -27,22 +29,27 @@ def voice_processing(message):
     audio = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(TOKEN, file_info.file_path)).content
     print(audio)
 
-    with open("audio_file.Ogg", "wb") as file:
+    with open("uploads/bot/audio_file.Ogg", "wb") as file:
         file.write(audio)
     subprocess.call(
         ['ffmpeg',
          '-i',
-         'audio_file.Ogg',
+         'uploads/bot/audio_file.Ogg',
          '-y',
          'audio_file.wav',
          '-hide_banner'])  # Перезаписываем с нужной кодировкой
 
     wavfiles = 'audio_file.wav'  # Имя очередного файла
-    prediction = get_prediction(wavfiles, model, CLASSES)  # Предсказание модели
+    #prediction = get_prediction(wavfiles, model, CLASSES)  # Предсказание модели
+    prediction = 'тестовый предикт bot'
     print(prediction)
     bot.send_message(chat_id=chat_id, text=prediction)
 
 
 if __name__ == '__main__':
+    bot.skip_pending = True
+    bot.remove_webhook()
     print('bot запущен...')
     bot.polling()
+
+print('Ну что-то есть')
