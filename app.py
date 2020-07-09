7e8370ac1fbb2274573e818fd05b3f1c93bace57
bolanebyla <alexleskov2000@gmail.com>
2020-07-09 11:19:06 +0800

    Замена model на prediction, переименование функции

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
-import os
 import flask
 import json
-import pickle
 
 from flask import request
 from flask import render_template
 from bot import *
 
-from model import get_prediction
+from prediction import get_prediction_text
 
 app = flask.Flask(__name__, template_folder='templates', static_folder='static')
-
-
-
-uploads_dir = os.path.join(app.instance_path, 'uploads')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -32,16 +26,17 @@ def index():
              '-hide_banner'])  # Перезаписываем с нужной кодировкой
 
         wavfiles = 'uploads/web/recording.wav'  # Имя очередного файла
-        prediction = get_prediction(wavfiles)  # Предсказание модели
-        print(prediction)
-        data = json.dumps({'data': prediction})  # Записываем в json
+        prediction_text, class_name = get_prediction_text(wavfiles)  # Предсказание модели
+        print(prediction_text, class_name)
+        data = json.dumps({'data': prediction_text})  # Записываем в json
         return data
+
 
 @app.route('/bot/' + TOKEN, methods=['GET', 'POST'])
 def webhook():
-    print('пришло', request)
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return 'ok', 200
+
 
 if __name__ == '__main__':
     app.run()
